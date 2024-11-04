3fea66d553291ebcfe23948c1f8bb489e59c7670
matiasbiagetti <biagettimati@gmail.com>
2024-11-04 10:11:00 -0300

    add posts, likes and comments to user response

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -9,25 +9,32 @@ class UserRepository {
   async findUserByEmail(email: string): Promise<IUser | null> {
     return await User.findOne({ email })
       .populate('followers', '_id firstName lastName username tier profileImage')
-      .populate('following', '_id firstName lastName username tier profileImage');
+      .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes');
   }
 
   async findUserByUsername(username: string): Promise<IUser | null> {
     return await User.findOne({ username })
       .populate('followers', '_id firstName lastName username tier profileImage')
-      .populate('following', '_id firstName lastName username tier profileImage');
+      .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes');
+
   }
 
   async findUserById(userId: string): Promise<IUser | null> {
     return await User.findById(userId)
       .populate('followers', '_id firstName lastName username tier profileImage')
-      .populate('following', '_id firstName lastName username tier profileImage');
+      .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes');
+
   }
 
   async updateUser(userId: string, data: Partial<IUser>): Promise<IUser | null> {
     return await User.findByIdAndUpdate(userId, data, { new: true })
       .populate('followers', '_id firstName lastName username tier profileImage')
-      .populate('following', '_id firstName lastName username tier profileImage');
+      .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes');
+
   }
 
   async deleteUser(userId: string): Promise<IUser | null> {
@@ -35,8 +42,7 @@ class UserRepository {
   }
 
   async searchUsers(searchQuery: string): Promise<IUser[]> {
-    const query = String(searchQuery); // Ensure searchQuery is a string
-    console.log(`Executing search with query: ${query}`); // Log the query
+    const query = String(searchQuery); 
 
     const users = await User.find({
       $or: [
@@ -48,16 +54,17 @@ class UserRepository {
     })
       .populate('followers', '_id firstName lastName username tier profileImage')
       .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes')
       .exec();
-
-    console.log(`Found users: ${JSON.stringify(users)}`); // Log the found users
     return users;
   }
 
   async followUser(userId: string, userToFollow: string): Promise<IUser | null> {
     const user = await User.findByIdAndUpdate(userToFollow, { $addToSet: { followers: userId } }, { new: true })
       .populate('followers', '_id firstName lastName username tier profileImage')
-      .populate('following', '_id firstName lastName username tier profileImage');
+      .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes')
+
 
     await User.findByIdAndUpdate(userId, { $push: { following: userToFollow } });
     return user;
@@ -66,7 +73,8 @@ class UserRepository {
   async unfollowUser(userId: string, userToUnfollow: string): Promise<IUser | null> {
     const user = await User.findByIdAndUpdate(userToUnfollow, { $pull: { followers: userId } }, { new: true })
       .populate('followers', '_id firstName lastName username tier profileImage')
-      .populate('following', '_id firstName lastName username tier profileImage');
+      .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes')
 
     await User.findByIdAndUpdate(userId, { $pull: { following: userToUnfollow } });
     return user
@@ -75,7 +83,8 @@ class UserRepository {
   async getAllUsers(): Promise<IUser[]> {
     return await User.find()
       .populate('followers', '_id firstName lastName username tier profileImage')
-      .populate('following', '_id firstName lastName username tier profileImage');
+      .populate('following', '_id firstName lastName username tier profileImage')
+      .populate('posts', '_id text media createdAt updatedAt likes comments').populate('likes', '_id username profileImage').populate('comments', '_id text media createdAt updatedAt likes')
   }
 }
 
